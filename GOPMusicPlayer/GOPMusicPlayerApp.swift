7694b56b2da3865e9d9346493711aab1f63bce0f
Rizqi Widianto <widiantorizqi@gmail.com>
2026-02-20 11:30:53 +0700

    Initial Commit

---
+++ GOPMusicPlayer/GOPMusicPlayerApp.swift
@@ -0,0 +1,17 @@
+//
+//  GOPMusicPlayerApp.swift
+//  GOPMusicPlayer
+//
+//  Created by user on 20/02/26.
+//
+
+import SwiftUI
+
+@main
+struct GOPMusicPlayerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
